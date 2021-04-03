693e53e93e87c1026e3013f917c3af4aad9d294a
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-04-03 00:26:55 -0300

    array of messages created, random message shower created

--- src/App.js
+++ src/App.js
@@ -2,10 +2,30 @@ import { Notifyer } from "./Notifyer.js";
 import { Timer } from "./Timer.js";
 import { Emitter } from "./Emitter.js";
 
-const notify = Notifyer.notify({
-  title: "Hora de se Hidratar",
-  body: "Relaxe e beba um pouco de água!",
-});
+const messages = [
+  "Relaxe e beba um pouco de água!",
+  "Descanse um pouquinho e se hidrate!",
+  "Hora de beber água e dar uma pequena pausa!",
+  "Trabalhando como nunca se hidratando como sempre!",
+  "Que tal beber água antes de continuar a tarefa?",
+  "Dei-me um pouco da água da vida!",
+  "Se você está lendo, então beba um pouquinho d'água!",
+  "Faça uma pequena pausa para se hidratar!"
+]
+
+const notify = () => {
+
+  const randomIndex = Math.floor(Math.random() * messages.length)
+
+  const notification = Notifyer.notify({
+    title: "Hora de se Hidratar",
+    body: messages[randomIndex],
+  });
+
+  notification()
+
+}
+
 const time = 25 * 60;
 
 const App = {
